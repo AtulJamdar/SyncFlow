04784666cd3af9f2048319d2f6787987c48cfe0d
AtulJamdar <work.atujamdar@gmail.com>
2025-10-02 14:19:35 +0530

    Adding forgot password functionality

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,16 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://syncflow-backend-cbau.onrender.com/api",
-    withCredentials: true,
+  baseURL: "https://syncflow-backend-cbau.onrender.com/api",
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+apiClient.get("/api/protected", {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
 
 export default apiClient;
