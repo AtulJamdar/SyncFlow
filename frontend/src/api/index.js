4ed7d96e53c4db6b61615bd4a053770e6d48715a
AtulJamdar <work.atujamdar@gmail.com>
2025-10-03 01:10:35 +0530

    fixing Forgot password functionality

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,6 +7,8 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
+  console.log("JWT Token:", token);
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
